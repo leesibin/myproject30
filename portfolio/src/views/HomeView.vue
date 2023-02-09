f2980affb004ea12d2a8ef6f75be37208610b722
leesibin <tlqls123@gmail.com>
2023-02-09 14:56:12 +0900

    2/09 틀만 만들기

---
+++ portfolio/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d1">여름</div>
+  <div class="d2"></div>
+  <div class="d3"></div>
+  <div class="d4"></div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
