258f17c35b689c2fa843a87687d420c3cdcd76df
leesibin <tlqls123@gmail.com>
2023-03-08 12:16:19 +0900

    3/8저장

--- portfolio/src/components/todolist.vue
+++ portfolio/src/components/todolist.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="todolist">
-    <div id="container" v-for="data in todos" :key="data.id">
-      <li id="text">{{ data }}</li>
+   
     </div>
   </div>
 </template>
--- portfolio/src/views/page1.vue
+++ portfolio/src/views/page1.vue
@@ -2,12 +2,17 @@
   <h2>To-do-list만들기</h2>
   <input type="text" v-model="content" @keyup.enter="addToDo()" />
   <button @click="addToDo()">제출</button>
-  <todolist :todos="todos" />
+  <button @click="allclear()">전체닫기</button>
+
+  <li id="text" v-for="list in todos" :key="list">
+    <input type="checkbox" v-bind:id="checkd" />
+    <label v-bind:for="checkd"></label>
+    {{ list }} <button>닫기</button>
+  </li>
   <!-- 컴포넌트 데이터끌어오기 -->
 </template>
 
 <script>
-import todolist from '../components/todolist.vue'
 /* eslint-disable */
 export default {
   name: 'app',
@@ -17,15 +22,19 @@ export default {
       content: ''
     }
   },
+  created() {},
   methods: {
-    addToDo: function () {
+    addToDo() {
       console.log('성공')
-      this.todos.push(this.content)
+      this.todos.push({ item: this.content })
       this.content = ''
+      localStorage.setItem('ToDolist', JSON.stringify(this.todos))
+    },
+    allclear() {
+      console.log('성공')
+      this.todos = ''
+      localStorage.clear()
     }
-  },
-  components: {
-    todolist
   }
 }
 </script>
