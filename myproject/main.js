616a07022ac89a135adf1b6deb48669cd67f4f65
leesibin <tlqls123@gmail.com>
2023-03-10 17:29:20 +0900

    3/10 skill 진행중

--- myproject/main.js
+++ myproject/main.js
@@ -11,3 +11,21 @@ window.onload = () =>{
       if (scrollpos >= awards.offsetHeight + 700) add_class_on_scroll(awards) 
     }) 
 } 
+
+function navigo (){
+  const header = document.querySelector('header'); //헤더부분획득
+  const headerheight = header.clientHeight;//헤더높이
+document.addEventListener('scroll', onScroll, { passive: true });//스크롤 이벤트
+ function onScroll () {
+     const scrollposition = pageYOffset;//스크롤 위치
+   const nav = document.querySelector('nav');//네비게이션
+   if (headerheight<=scrollposition){//만약 헤더높이<=스크롤위치라면
+     nav.classList.add('.header-menu')//fix클래스를 네비에 추가
+   }
+   else {//그 외의 경우
+     nav.classList.remove('fix');//fix클래스를 네비에서 제거
+   }
+ } 
+  
+}
+navigo()
