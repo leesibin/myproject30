b2493960369bbbc058e9063940f7d28d39fa4b43
leesibin <tlqls123@gmail.com>
2023-03-14 16:20:41 +0900

    3/14 파일정리

--- myproject/main.js
+++ myproject/main.js
@@ -2,14 +2,14 @@ window.onload = () =>{
     let scrollpos = window.scrollY; 
     const about = document.querySelector("#about > div")
     const tech = document.querySelector("#skills > div")
-    const projects = document.querySelector("#projects > div")
     const awards = document.querySelector("#awards > div") 
+    const projects = document.querySelector("#projects > div")
     const add_class_on_scroll = (dom) => dom.classList.add("come-in")  
     window.addEventListener('scroll', function() { 
       scrollpos = window.scrollY; 
-      if (scrollpos >= about.offsetHeight - 200) add_class_on_scroll(about)
-      if (scrollpos >= tech.offsetHeight + 100) add_class_on_scroll(tech)
-      if (scrollpos >= projects.offsetHeight + 500) add_class_on_scroll(projects) 
-      if (scrollpos >= awards.offsetHeight + 700) add_class_on_scroll(awards) 
+      if (scrollpos >= about.offsetHeight - 100) add_class_on_scroll(about)
+      if (scrollpos >= tech.offsetHeight + 300) add_class_on_scroll(tech)
+      if (scrollpos >= awards.offsetHeight + 500) add_class_on_scroll(awards) 
+      if (scrollpos >= projects.offsetHeight + 700) add_class_on_scroll(projects)
     }) 
 } 
